cbae9cffebe8c74efd92c6c49b06e2899a4e08ce
Nikita Mislivets <nikitamislivets@gmail.com>
2026-09-18 22:09:05 +0300

    create method CreateUserSplDeposit

--- programs/my_counter/src/lib.rs
+++ programs/my_counter/src/lib.rs
@@ -16,29 +16,43 @@ pub mod my_liquidity_bank {
     use crate::update_is_paused_status::{update_is_paused, UpdateIsPaused};
     use crate::deposit_sol::{process_deposit_sol, DepositSol};
     use crate::create_bank::{process_create_bank, CreateBank};
+    use crate::create_users_spl_deposit::{process_create_user_spl_deposit, CreateUserSplDeposit};
     use super::*;
 
     pub fn initialize_market(ctx: Context<InitializeMarket>, protocol_fee_bps: u16,) -> Result<()> {
         process_initialize_market(ctx, protocol_fee_bps)
     }
 
-    pub fn deposit_native_sol(ctx: Context<DepositSol>, amount: u64,) -> Result<()> {
-        process_deposit_sol(ctx, amount)
-    }
-
     pub fn create_bank_for_token(ctx: Context<CreateBank>,) -> Result<()> {
         process_create_bank(ctx)
     }
 
-    pub fn create_bank_for_native_sol(ctx: Context<CreateBank>,) -> Result<()> {
-        process_create_bank(ctx)
+    pub fn create_bank_for_native_sol(ctx: Context<CreateBankNativeSolana>,) -> Result<()> {
+        process_create_bank_for_native_solana(ctx)
     }
+
+    pub fn deposit_native_sol(ctx: Context<DepositSol>, amount: u64,) -> Result<()> {
+        process_deposit_sol(ctx, amount)
+    }
+
+    pub fn create_user_spl_deposit(ctx: Context<CreateUserSplDeposit>,) -> Result<()> {
+        process_create_user_spl_deposit(ctx, )
+    }
+
+
+    // pub fn withdraw_native_sol(ctx: Context<WithdrawNativeSol>, amount: u64,) -> Result<()> {
+    //     todo!();
+    // }
 
     // pub fn withdraw(ctx: Context<InitializeMarket>, amount: u64, currency: Currency) -> Result<()> {
     //     todo!();
     // }
     //
     // pub fn borrow(ctx: Context<InitializeMarket>, amount: u64, currency: Currency) -> Result<()> {
+    //     todo!();
+    // }
+
+    // pub fn borrow_native_sol(ctx: Context<InitializeMarket>, amount: u64, currency: Currency) -> Result<()> {
     //     todo!();
     // }
 
